57947757e8284e725b47f270f9305ffccbacacdd
andersstarup <112627350+andersstarup@users.noreply.github.com>
2022-10-07 11:02:36 +0200

    Communication test

--- MyFirstWPFApplication/MainWindow.xaml.cs
+++ MyFirstWPFApplication/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace MyFirstWPFApplication
     public partial class MainWindow : Window
     {
         [StructLayout(LayoutKind.Sequential, Pack = 4)] // creates the least padding, due to the program now does the most efficient allignment
+
         struct UdpPack
         {
             public byte addr;
@@ -27,6 +28,7 @@ namespace MyFirstWPFApplication
             public int op2;
             public UInt16 Value;
         }
+
         static byte[] getBytes(UdpPack str)
         {
             int size = Marshal.SizeOf(str);
@@ -51,12 +53,12 @@ namespace MyFirstWPFApplication
         UdpClient Client = new UdpClient();
 
         IPEndPoint localEp;
+        IPEndPoint localEpSTEP;
+        IPEndPoint localEpLED;
 
-      
         public MainWindow()
         {
             InitializeComponent();
-           
         }
         private void Board1Selector_Click(object sender, RoutedEventArgs e)
         {
@@ -70,11 +72,14 @@ namespace MyFirstWPFApplication
             Toggles.Visibility = Visibility.Collapsed;
             NumbRot.Visibility = Visibility.Collapsed;
             Direction.Visibility = Visibility.Collapsed;
+            ComTest.Visibility = Visibility.Collapsed;
             Freq.Visibility = Visibility.Collapsed;
             LED.Visibility = Visibility.Visible;
             Fork.Visibility = Visibility.Visible;
+            StepM.Visibility = Visibility.Visible;
             FuncSelect.Visibility = Visibility.Visible;
         }
+
         private void Board2Selector_Click(object sender, RoutedEventArgs e)
         {
             BoardSel.Content = "LED Board";
@@ -87,24 +92,27 @@ namespace MyFirstWPFApplication
             Toggles.Visibility = Visibility.Collapsed;
             NumbRot.Visibility = Visibility.Collapsed;
             Direction.Visibility = Visibility.Collapsed;
+            ComTest.Visibility = Visibility.Collapsed;
             Freq.Visibility = Visibility.Collapsed;
             LED.Visibility = Visibility.Visible;
             Fork.Visibility = Visibility.Visible;
+            StepM.Visibility = Visibility.Visible;
             FuncSelect.Visibility = Visibility.Visible;
-
-
         }
-        private void LED_Click(object sender, RoutedEventArgs e)
+
+        private void BothBoards_click(object sender, RoutedEventArgs e)
         {
-            FuncSel.Content = "LED";
-            Direction.Visibility = Visibility.Collapsed;
+            ComTest.Visibility = Visibility.Visible;
+            Toggles.Visibility = Visibility.Collapsed;
             NumbRot.Visibility = Visibility.Collapsed;
+            Direction.Visibility = Visibility.Collapsed;
             Freq.Visibility = Visibility.Collapsed;
-            Toggles.Visibility = Visibility.Visible;
-
-
-            UdpOut.addr = 1;
+            LED.Visibility = Visibility.Collapsed;
+            Fork.Visibility = Visibility.Collapsed;
+            StepM.Visibility = Visibility.Collapsed;
+            FuncSelect.Visibility = Visibility.Visible;
         }
+
         private void StepM_Click(object sender, RoutedEventArgs e)
         {
             FuncSel.Content = "Stepper motor";
@@ -116,6 +124,19 @@ namespace MyFirstWPFApplication
 
             UdpOut.addr = 2;
         }
+
+        private void LED_Click(object sender, RoutedEventArgs e)
+        {
+            FuncSel.Content = "LED";
+            Direction.Visibility = Visibility.Collapsed;
+            NumbRot.Visibility = Visibility.Collapsed;
+            Freq.Visibility = Visibility.Collapsed;
+            Toggles.Visibility = Visibility.Visible;
+
+
+            UdpOut.addr = 1;
+        }
+
         private void Fork_click(object sender, RoutedEventArgs e)
         {
             FuncSel.Content = "Fork sensor";
@@ -125,6 +146,65 @@ namespace MyFirstWPFApplication
             Freq.Visibility = Visibility.Collapsed;
 
             UdpOut.addr = 3;
+        }
+
+        private async void Com_test_click(object sender, RoutedEventArgs e)
+        {
+            FuncSel.Content = "Com Test"; 
+           
+            localEpSTEP  = new IPEndPoint(IPAddress.Parse("192.168.1.123"), 70);
+            localEpLED = new IPEndPoint(IPAddress.Parse("192.168.1.124"), 69);
+
+            /////////////////////////////////////////////////////////// SEND AND RECIEVE TIL STEP BOARD
+            UdpOut.addr = 2;
+            UdpOut.op1 = 2;
+            Client.Connect(localEpSTEP);
+            byte[] bytesent = getBytes(UdpOut);
+            Client.Send(bytesent, bytesent.Length);
+
+            var dataRecieved = await Client.ReceiveAsync();
+            string text = Encoding.UTF8.GetString(dataRecieved.Buffer);
+
+            Client.Close();
+
+
+            Scroller.Content += "Message recieved from " + dataRecieved.RemoteEndPoint + ": " + text + Environment.NewLine;
+
+            Scroller.ScrollToBottom();
+            ///////////////////////////////////////////////////////////
+
+            /////////////////////////////////////////////////////////// SEND AND RECIEVE TIL LED BOARD
+            UdpOut.addr = 4;
+            Client.Connect(localEpLED);
+            bytesent = getBytes(UdpOut);
+            Client.Send(bytesent, bytesent.Length);
+
+            dataRecieved = await Client.ReceiveAsync();
+            text = Encoding.UTF8.GetString(dataRecieved.Buffer);
+
+            Client.Close();
+
+
+            Scroller.Content += "Message recieved from " + dataRecieved.RemoteEndPoint + ": " + text + Environment.NewLine;
+
+            Scroller.ScrollToBottom();
+            ///////////////////////////////////////////////////////////
+
+            /////////////////////////////////////////////////////////// SEND AND RECIEVE TIL STEP BOARD
+            Client.Connect(localEpSTEP);
+            bytesent = getBytes(UdpOut);
+            Client.Send(bytesent, bytesent.Length);
+
+            dataRecieved = await Client.ReceiveAsync();
+            text = Encoding.UTF8.GetString(dataRecieved.Buffer);
+
+            Client.Close();
+
+
+            Scroller.Content += "Message recieved from " + dataRecieved.RemoteEndPoint + ": " + text + Environment.NewLine;
+
+            Scroller.ScrollToBottom();
+            ///////////////////////////////////////////////////////////
         }
 
         private void StepB_Click(object sender, RoutedEventArgs e)
@@ -171,7 +251,6 @@ namespace MyFirstWPFApplication
 
                 Scroller.Content += "Message recieved from " + dataRecieved.RemoteEndPoint + ": " + text + Environment.NewLine;
 
-                //Scroller.Content += "Message recieved from " + localEp + ": " + text + Environment.NewLine;
                 Scroller.ScrollToBottom();
                 
                
@@ -183,15 +262,18 @@ namespace MyFirstWPFApplication
             }
 
         }
+
         private void slRotations_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             UdpOut.op2 = (int)slRotations.Value;
            // Debug.WriteLine(hej);
         }
+
         private void slFreq_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             UdpOut.Value = (UInt16)SlFreq.Value;
         }
+
         private void slToggle_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             UdpOut.op1 = (byte)slToggle.Value;
@@ -203,5 +285,4 @@ namespace MyFirstWPFApplication
             Scroller.ScrollToBottom();
         }
     }
-    
 }
